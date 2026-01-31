a9593769bfc530337a106444f32b097645351cd3
shadow <gregoyaga@gmail.com>
2026-01-31 12:19:15 +0100

    verification du logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export default function Navbar() {
     return (
-        <Contenaire className='p-3'>
-            <div><Image src={'/logo.png'} alt={'logo'}/></div>
+        <Contenaire className='stiky top-0 left-0 bg-red-400'>
+            <div><Image src={'/logo.png'} alt={'logo'} width={70} height={70} /></div>
         </Contenaire>
     )
 }
