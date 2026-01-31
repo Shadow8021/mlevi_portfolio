bd70ac8f2f8c3aff6975d38ce69ec2ffb1a97710
shadow <gregoyaga@gmail.com>
2026-01-31 12:09:42 +0100

    ajout et test du logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import Contenaire from './Contenaire'
+import Image from 'next/image'
+
 export default function Navbar() {
     return (
         <Contenaire className='p-3'>
-            <div>hshsh</div>
+            <div><Image src={'/logo.png'} alt={'logo'}/></div>
         </Contenaire>
     )
 }
