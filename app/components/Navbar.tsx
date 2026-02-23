958cad2969c22df17f1c6ef292479a28e2d957f4
Shadow821 <gregoyaga@gmail.com>
2026-02-23 21:20:04 +0100

    position de la navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Navbar() {
     return (
-        <Contenaire className='stiky top-0 left-0 bg-red-400'>
+        <Contenaire className='sticky left-0 top-0 w-full h-20 flex items-center justify-evenly bg-red-400'>
             <div><Image src={'/logo.png'} alt={'logo'} width={70} height={70} /></div>
         </Contenaire>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar";
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen  justify-center bg-zinc-50 font-sans dark:bg-black">
       <Navbar />
     </div>
   );
