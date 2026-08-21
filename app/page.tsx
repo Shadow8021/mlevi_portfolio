b2f067473f63a9f503457162de3c802a7e272708
Shadow8021 <gregoyaga@gmail.com>
2026-08-21 11:42:57 +0100

    update card

--- app/page.tsx
+++ app/page.tsx
@@ -70,17 +70,20 @@ export default function Home() {
       alt="Shoes" />
   </figure>
   <div className="card-body">
-    <h2 className="card-title">
-      Card Title
-      <div className="badge badge-secondary">NEW</div>
-    </h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
-    </div>
+    <div className="project-content">
+                <p className="project-type">{project.type}</p>
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+                <div className="tags">{project.tags.map((tag) =>( 
+                  <span key={tag}>{tag}</span>))}
+                </div>
+              </div>
+   
   </div>
 </div>
+
+
+
 
 
 
